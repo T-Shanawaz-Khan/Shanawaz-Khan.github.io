13c316a7b7eface565806a288b336b0622932ad7
T-Shanawaz-Khan <140812352+T-Shanawaz-Khan@users.noreply.github.com>
2023-07-28 18:12:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 /*typed js */
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Ethical Hacker', 'Java Developer'],
+    strings: ['Software Engineer', 'Web Developer', 'Java Developer'],
             typeSpeed: 100,
             backSpeed: 100,
             backDelay: 1000,
